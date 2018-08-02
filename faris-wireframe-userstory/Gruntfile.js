22c81c4dd61d0ae488d5b7e54fa8dfca1f457f00
Faris Tangastani <ftangastani@gmail.com>
2018-08-02 13:30:18 +0200

    chore: moved gruntfile and introduced another package.json

--- faris-wireframe-userstory/Gruntfile.js
+++ faris-wireframe-userstory/Gruntfile.js
@@ -1,13 +1,27 @@
-module.exports = function(grunt) {
 
+module.exports = function(grunt) {
+const sass = require('node-sass');
     //config
     grunt.initConfig({
-
+        sass: {
+            build: {
+                options: {
+                    implementation: sass,
+                    sourceMap: true
+                },
+                files: [{
+                    src:'public/main.scss',
+                    dest:'public/main.css'
+                }]
+            }
+        }
     })
     //Load plugins
-    // grunt.loadNpmTasks('')
+    // grunt.loadNpmTasks('grunt-contrib-concat')
+    grunt.loadNpmTasks('grunt-sass')
 
     //register tasks
+    grunt.registerTask('default', ['sass']);
     grunt.registerTask('run', function(){
         console.log('Grunt is up and running')
     })
@@ -16,4 +30,6 @@ module.exports = function(grunt) {
     })
 
     grunt.registerTask('all',['sleep','run'])
+
+
 };
